b425c20aa1924eaec4a35a4fe21cba6fddf834e5
shaktidhara <shaktidhara@users.noreply.github.com>
2018-10-03 16:39:22 +0530

    Update connection.tf

--- connection.tf
+++ connection.tf
@@ -1,6 +1,6 @@
 #AWS creds configuration. 
-#provider "aws" {
-#  region                  = "eu-west-1"
- # shared_credentials_file = "~/.aws/credentials"
-  #profile                 = "customprofile"
-#}
+provider "aws" {
+  region                  = "eu-west-1"
+  shared_credentials_file = "~/.aws/credentials"
+  profile                 = "customprofile"
+}
