f475664ab14950bd074255176cbb1c9b13157990
shaktidhara <shaktidhara@users.noreply.github.com>
2018-10-03 16:47:20 +0530

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -1,8 +1,8 @@
-resource "aws_vpc" "environment-example-network" {
-	cidr_block = "11.0.0.0/16"
+resource "aws_vpc" "environment-example-network2" {
+	cidr_block = "12.0.0.0/16"
 	enable_dns_hostnames = true
 	enable_dns_support = true
 	tags{
-		Name = "terraform-aws-vpc-exmpls"
+		Name = "terraform-aws-vpc-exmpls2"
 	}
 }
