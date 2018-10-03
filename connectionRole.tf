c18d9dbc2fd7919382adbb28959bd8a1b1f2015d
shaktidhara <shaktidhara@users.noreply.github.com>
2018-10-03 16:38:44 +0530

    Update connectionRole.tf

--- connectionRole.tf
+++ connectionRole.tf
@@ -1,7 +1,7 @@
-provider "aws" {
-  assume_role {
-    role_arn     = "arn:aws:iam::270473472622:role/terraform_role"
-    session_name = "AWSTeraform"
-    region       = "eu-west-1"
-  }
-}
+#provider "aws" {
+ # assume_role {
+  #  role_arn     = "arn:aws:iam::270473472622:role/terraform_role"
+   # session_name = "AWSTeraform"
+    #region       = "eu-west-1"
+  #}
+#}
