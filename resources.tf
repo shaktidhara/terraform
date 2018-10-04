cb107055af61a62fca7a3cf75f2b21f34037b657
smuduli4052 <smuduli4052>
2018-10-04 05:49:41 +0000

    apple

--- resources.tf
+++ resources.tf
@@ -1,8 +1,14 @@
-resource "aws_vpc" "environment-example-network2" {
+resource "aws_vpc" "environment-example-network" {
 	cidr_block = "11.0.0.0/16"
 	enable_dns_hostnames = true
 	enable_dns_support = true
 	tags{
-		Name = "terraform-aws-vpc-exmpls2"
+		Name = "terraform-aws-vpc-exmpls"
 	}
+}
+
+resource "aws_subnet" "subnet1" {
+	cidr_block="${cidrsubnet(aws_vpc.environment-example-network.cidr_block, 3, 1)}"
+	vpc_id = "${aws_vpc.environment-example-network.id}"
+	availability_zone="eu-east-1"
 }
