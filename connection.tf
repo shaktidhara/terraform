92fe6d1db44635d8e261a66f7e52f22d89ac981b
shaktidhara <shaktidhara@users.noreply.github.com>
2018-10-04 11:54:06 +0530

    Update connection.tf

--- connection.tf
+++ connection.tf
@@ -8,6 +8,6 @@
 
 provider "aws" {
   region     = "eu-west-1"
-  access_key = "AKIAIMCMOPN3WA6TSKCA"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
